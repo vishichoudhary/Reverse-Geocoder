19ae5830bd2be0c6184d335e2aaf0865f163e80b
vishichoudhary <vishal129.vk@gmail.com>
2019-05-15 22:40:10 +0530

    parse input string correctly

--- reverseGeoCoder.py
+++ reverseGeoCoder.py
@@ -1,3 +1,4 @@
+import hashlib
 import sqlite3
 conn = sqlite3.connect('cordinatedToAddress.db')
 cursor = conn.cursor()
@@ -5,10 +6,11 @@ cursor = conn.cursor()
 coordinatesTable = """ CREATE TABLE IF NOT EXISTS coordinates(
                                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                                     lat VARCHAR(10),
-                                    long VARCHAR(10),
+                                    lng VARCHAR(10),
                                     address VARCHAR(50)
                                 ); """
 cursor.execute(coordinatesTable)
+
 
 class ReverseGeoCoder:
 
@@ -16,20 +18,39 @@ class ReverseGeoCoder:
         self.coordinates = []
 
     def registerCoordintes(self):
-        self.coordinates.append(['28.657776','77.289506'])
-        self.coordinates.append(['28.657776','77.289506'])
+        coordinates = input()
+        if not coordinates[0] == "[":
+            coordinates = input().split(",")
+            self.coordinates.append([coordinates[0][1:], coordinates[1][:-1]])
+        else:
+            coordinates = coordinates[1:-1].split(',')
+            for index, coordinate in enumerate(coordinates):
+                if index % 2 == 1:
+                    self.coordinates.append(
+                        [coordinates[index-1][1:], coordinates[index][:-1]])
+
+    def fetchAddress(self, lat, lng):
+        # call maps api here, but we are faking api call for randrom address
+        hash_object = hashlib.md5((lat + lng).encode())
+        return hash_object.hexdigest()
 
     def run(self):
+        mappedAddress = {}
         for coordinate in self.coordinates:
-            cursor.execute('SELECT * FROM coordinates WHERE lat=? and long=?' % (coordinate[0], coordinate[1]))
+            cursor.execute(
+                'SELECT * FROM coordinates WHERE lat=? and lng=?', (coordinate[0], coordinate[1]))
             result = cursor.fetchone()
             if result == None:
                 # cache the result
-                cursor.execute('insert into coordinates(lat, long, address) values(?, ?, ?);' , (coordinate[0], coordinate[1], "random hu maai"))
+                address = self.fetchAddress(coordinate[0], coordinate[1])
+                cursor.execute('insert into coordinates(lat, lng, address) values(?, ?, ?);', (
+                    coordinate[0], coordinate[1], address))
+                mappedAddress[coordinate[0] + "," + coordinate[1]] = address
             else:
-                print(result)
+                mappedAddress[coordinate[0] + "," + coordinate[1]] = result[3]
+        print(mappedAddress)
+
 
 reverseGeoCoder = ReverseGeoCoder()
 reverseGeoCoder.registerCoordintes()
 reverseGeoCoder.run()
-
